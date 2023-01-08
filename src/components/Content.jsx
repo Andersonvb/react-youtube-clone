c7003ccef488d342327dc1deab54cd3f4a25a8e3
Andersonvb <andersonvalenciabermudez@gmail.com>
2023-01-08 09:57:44 -0500

    Make Videos Container responsive

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,13 +6,37 @@ import DogImg from "../images/dog.jpg"
 const Content = () => {
   return (
     <div className="content-container">
-      <Video title="Los PELIGROS de la GRATIFICACIÓN INMEDIATA" channel="La Ducha Fría" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
 
-      <Video title="Los PELIGROS de la GRATIFICACIÓN INMEDIATA" channel="La Ducha Fría" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
 
-      <Video title="Los PELIGROS de la GRATIFICACIÓN INMEDIATA" channel="La Ducha Fría" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+    
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
 
-      <Video title="Los PELIGROS de la GRATIFICACIÓN INMEDIATA" channel="La Ducha Fría" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
+
+      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
     </div>
   );
 };
