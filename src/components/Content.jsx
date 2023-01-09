bea5aab20cd8ad3ebb548e5647c953a9360f0935
Andersonvb <andersonvalenciabermudez@gmail.com>
2023-01-09 09:29:01 -0500

    Fix hover colors

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,35 +6,6 @@ import DogImg from "../images/dog.jpg"
 const Content = () => {
   return (
     <div className="content-container">
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-    
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={3500} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
-      <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
-
       <Video title="Te amo MUCHO hermosa" channel="Dante Feli" views={2300} time="6 hours ago" img={DogImg} channelImg={DogImg}/>
     </div>
   );
